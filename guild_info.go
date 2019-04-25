d9f0aa37e094181298f6d823343e8583de6f9762
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-04-25 15:11:30 -0700

    add GuildInfo and TeamInfo, stores config and spreadsheet

---
+++ guild_info.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bigheadgeorge/thonky2/db"
+	spreadsheet "gopkg.in/Iwark/spreadsheet.v2"
+)
+
+// BaseInfo acts as the crappy interface for TeamInfo and GuildInfo, but it works :)
+type BaseInfo interface {
+	SheetLink() string
+}
+
+// TeamInfo stores info about a team
+type TeamInfo struct {
+	Sheet *spreadsheet.Spreadsheet
+	*db.TeamConfig
+}
+
+// SheetLink returns the link to the team's sheet
+func (t *TeamInfo) SheetLink() string {
+	return "https://docs.google.com/spreadsheets/d/" + t.DocKey.String
+}
+
+// GuildInfo stores the default TeamInfo for the guild, plus info on every team in the guild
+type GuildInfo struct {
+	Teams []*TeamInfo
+	*TeamInfo
+}
+
+// GetGuildInfo returns info about a guild, including info about each of the teams in that guild.
+func GetGuildInfo(guildID string) (g *GuildInfo, err error) {
+	handler, err := db.NewHandler()
+	if err != nil {
+		return
+	}
+	defer handler.Close()
+
+	config, err := handler.GetGuild(guildID)
+	if err != nil {
+		return
+	}
+
+	getTeamInfo := func(config *db.TeamConfig) *TeamInfo {
+		var sheet spreadsheet.Spreadsheet
+		var err error
+		if config.DocKey.Valid {
+			sheet, err = Service.FetchSpreadsheet(config.DocKey.String)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		return &TeamInfo{&sheet, config}
+	}
+	g = &GuildInfo{TeamInfo: getTeamInfo(config)}
+	teams, err := handler.GetTeams(guildID)
+	if err != nil {
+		return
+	}
+	for _, teamConfig := range teams {
+		g.Teams = append(g.Teams, getTeamInfo(teamConfig))
+	}
+	return
+}
