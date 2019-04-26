473acd2e3b9820b3d699f4a13b1ed23cbe29ac8e
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-04-26 11:10:38 -0700

    move logging to a seperate file

--- bot.go
+++ bot.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"compress/gzip"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 	spreadsheet "gopkg.in/Iwark/spreadsheet.v2"
@@ -57,17 +53,6 @@ func init() {
 	}
 }
 
-func openLog(path string) (*os.File, error) {
-	if _, err := os.Open(path); os.IsNotExist(err) {
-		_, err = os.Create(path)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	return os.OpenFile(path, os.O_APPEND|os.O_WRONLY, os.ModeAppend|0644)
-}
-
 func main() {
 	d, err := discordgo.New("Bot " + Token)
 	if err != nil {
@@ -87,52 +72,14 @@ func main() {
 		panic(err)
 	}
 
-	if _, err := os.Open("logs"); os.IsNotExist(err) {
-		os.Mkdir("logs", 0700)
-	}
-	year, month, day := time.Now().Date()
-	logName := fmt.Sprintf("%d-%d-%d.log", year, month, day)
-	logPath := "logs/" + logName
-	logFile, err := openLog(logPath)
-	if err != nil {
-		panic(err)
-	}
-	log.SetOutput(io.MultiWriter(os.Stdout, logFile))
-
+	logFile := StartLog()
 	log.Println("running")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
+	CompressLog(logFile)
 	d.Close()
-
-	err = logFile.Close()
-	if err != nil {
-		panic(err)
-	}
-	f, err := openLog(logPath + ".gz")
-	if err != nil {
-		panic(err)
-	}
-	l, err := os.Open(logPath)
-	if err != nil {
-		panic(err)
-	}
-	b, err := ioutil.ReadAll(l)
-	if err != nil {
-		panic(err)
-	}
-	l.Close()
-	gz := gzip.NewWriter(f)
-	gz.Write(b)
-	err = gz.Close()
-	if err != nil {
-		panic(err)
-	}
-	err = os.Remove(logPath)
-	if err != nil {
-		panic(err)
-	}
 }
 
 func ready(s *discordgo.Session, r *discordgo.Ready) {
---
+++ logging.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+)
+
+func logPath() string {
+	year, month, day := time.Now().Date()
+	return fmt.Sprintf("logs/%d-%d-%d.log", year, month, day)
+}
+
+func openLog(path string) (*os.File, error) {
+	if _, err := os.Open(path); os.IsNotExist(err) {
+		_, err = os.Create(path)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return os.OpenFile(path, os.O_APPEND|os.O_WRONLY, os.ModeAppend|0644)
+}
+
+// StartLog sets up logging
+func StartLog() *os.File {
+	if _, err := os.Open("logs"); os.IsNotExist(err) {
+		os.Mkdir("logs", 0700)
+	}
+	logFile, err := openLog(logPath())
+	if err != nil {
+		panic(err)
+	}
+	log.SetOutput(io.MultiWriter(os.Stdout, logFile))
+	return logFile
+}
+
+// CompressLog replaces a log with a gzip-compressed log
+func CompressLog(logFile *os.File) {
+	err := logFile.Close()
+	if err != nil {
+		panic(err)
+	}
+	f, err := openLog(logPath() + ".gz")
+	if err != nil {
+		panic(err)
+	}
+	l, err := os.Open(logPath())
+	if err != nil {
+		panic(err)
+	}
+	b, err := ioutil.ReadAll(l)
+	if err != nil {
+		panic(err)
+	}
+	l.Close()
+	gz := gzip.NewWriter(f)
+	gz.Write(b)
+	err = gz.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = os.Remove(logPath())
+	if err != nil {
+		panic(err)
+	}
+
+}
