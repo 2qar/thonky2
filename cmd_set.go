f0924acb11977ce31460e30eda33a0a31008daa7
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-05-11 17:05:20 -0700

    start !set, parse args depending on whether player or week is given

--- cmd_set.go
+++ cmd_set.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"strings"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -27,6 +30,66 @@ func Set(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
 		return
 	}
 
+	if len(args) >= 3 {
+		day := dayInt(args[1])
+		if day != -1 {
+			// update w/ day
+			log.Printf("update day %q w/ index %d\n", args[1], day)
+			args, err := parseArgs(args[2:], info.Sheet.ValidActivities)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			log.Println(args)
+			return
+		}
+
+		var player *Player
+		playerName := strings.ToLower(args[1])
+		for _, p := range info.Players {
+			if playerName == strings.ToLower(p.Name) {
+				player = p
+			}
+		}
+
+		if player != nil {
+			day = dayInt(args[2])
+			if day != -1 {
+				// update w/ player
+				log.Printf("update player %q\n", player.Name)
+				args, err := parseArgs(args[3:], []string{"Yes", "Maybe", "No"})
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				log.Println(args)
+				return
+			}
+
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Invalid day %q", args[2]))
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Invalid day / player %q", args[1]))
+		return
+	}
+
+	s.ChannelMessageSend(m.ChannelID, "weird amount of args")
+}
+
+// dayInt gets a weekday int from a day name.
+func dayInt(dayName string) int {
+	day := -1
+	dayName = strings.ToLower(dayName)
+	for i := 0; i < 7; i++ {
+		currName := strings.ToLower(time.Weekday(i).String())
+		if dayName == currName || dayName[:3] == currName[:3] {
+			day = Weekday(i)
+			break
+		}
+	}
+	return day
+}
 
 // parseArgs takes a list of unformatted arguments and tries to match them with a given list of valid arguments.
 func parseArgs(args []string, validArgs []string) ([]string, error) {
