b7c820f491011e8cc4adf82bbaf20f57fabea16f
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-05-16 10:45:01 -0700

    fix bad fmt.Sprintf call

--- reminders.go
+++ reminders.go
@@ -58,7 +58,7 @@ func (r reminderCheck) Run() {
 }
 
 func addReminder(time int, s *discordgo.Session) error {
-	return scheduler.AddJob(fmt.Sprintf("0 %d 15-20 * * *"), reminderCheck{Time: time, Session: s})
+	return scheduler.AddJob(fmt.Sprintf("0 %d 15-20 * * *", time), reminderCheck{Time: time, Session: s})
 }
 
 // StartReminders starts checking for reminders 45 and 15 minutes before each hour
