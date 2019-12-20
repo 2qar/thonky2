8b0d69c0ad8e0161ea241205738de2809d86ed73
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-12-19 20:54:35 -0800

    add tournament stuff to gamebattles

--- gamebattles/gamebattles.go
+++ gamebattles/gamebattles.go
@@ -6,9 +6,13 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"strconv"
 )
 
-const apiv1 = "https://gb-api.majorleaguegaming.com/api/web/v1/"
+const (
+	apiv1    = "https://gb-api.majorleaguegaming.com/api/v1/"
+	apiv1Web = "https://gb-api.majorleaguegaming.com/api/web/v1/"
+)
 
 // Player info API
 // http://profile.majorleaguegaming.com/Tydra_/
@@ -70,7 +74,7 @@ func getEndpoint(link string) ([]byte, error) {
 
 // GetTeam gets a team.
 func GetTeam(id string) (Team, error) {
-	b, err := getEndpoint(apiv1 + "team-members-extended/team/" + id)
+	b, err := getEndpoint(apiv1Web + "team-members-extended/team/" + id)
 	if err != nil {
 		return Team{}, err
 	}
@@ -95,7 +99,7 @@ func GetTeam(id string) (Team, error) {
 		players = append(players, p.TeamMember)
 	}
 
-	b, err = getEndpoint(apiv1 + "team-screen/" + id)
+	b, err = getEndpoint(apiv1Web + "team-screen/" + id)
 	if err != nil {
 		return Team{}, err
 	}
@@ -122,5 +126,56 @@ func GetTeam(id string) (Team, error) {
 	return teamJSON.Body.TeamWithEligibilityAndPremiumStatus.Team, nil
 }
 
-// TODO: add a team search function
-// https://gamebattles.majorleaguegaming.com/pc/overwatch/tournament/Breakable-Barriers-NA-1/teams
+// GetTeams returns the teams participating in the tournament with the given ID.
+func GetTeams(tournamentID string) ([]Team, error) {
+	b, err := getEndpoint(apiv1 + "tournament-team/with-eligibility-and-premium-status/" + tournamentID + "?pageSize=1000&pageNumber=1")
+	if err != nil {
+		return []Team{}, err
+	}
+
+	teamsJSON := struct {
+		Errors []struct {
+			Code string
+		}
+		Body struct {
+			Records []Team
+		}
+	}{}
+
+	err = json.Unmarshal(b, &teamsJSON)
+	if err != nil {
+		return []Team{}, err
+	} else if len(teamsJSON.Errors) > 0 {
+		return []Team{}, fmt.Errorf("error getting teams in tournament %q: %s", tournamentID, teamsJSON.Errors[0].Code)
+	}
+
+	return teamsJSON.Body.Records, nil
+}
+
+// GetTournamentID gets the ID of the tournament with the given name.
+// `name` should be the name at the end of the url: the name for gamebattles.com/../../tournament/Breakable-Barriers would be "Breakable-Barriers"
+func GetTournamentID(name, game, platform string) (string, error) {
+
+	b, err := getEndpoint(apiv1 + "tournaments/simple-url/" + name + "/arena-simple-url/" + game + "/platform-simple-url/" + platform)
+	if err != nil {
+		return "", err
+	}
+
+	tournamentJSON := struct {
+		Errors []struct {
+			Code string
+		}
+		Body struct {
+			ID uint64
+		}
+	}{}
+
+	err = json.Unmarshal(b, &tournamentJSON)
+	if err != nil {
+		return "", err
+	} else if len(tournamentJSON.Errors) > 0 {
+		return "", fmt.Errorf("error getting tournament %q: %s", name, tournamentJSON.Errors[0].Code)
+	}
+
+	return strconv.FormatUint(tournamentJSON.Body.ID, 10), nil
+}
