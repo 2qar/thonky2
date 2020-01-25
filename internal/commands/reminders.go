48fae61ef952dcd10a4405c85af5426d9dbc0562
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2020-01-24 22:00:34 -0800

    add !reminders_set for updating reminder config

--- internal/commands/reminders.go
+++ internal/commands/reminders.go
@@ -3,6 +3,8 @@ package commands
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"regexp"
 	"strings"
 
 	"github.com/bigheadgeorge/thonky2/pkg/command"
@@ -12,10 +14,14 @@ import (
 )
 
 func init() {
-	command.AddCommand("reminders", "Get reminder config for this team.", [][2]string{{"!reminders", "Show the reminder config for this team."}}, Reminders)
+	examples := [][2]string{{"!reminders", "Show the reminder config for this team."}}
+	command.AddCommand("reminders", "Get reminder config for this team.", examples, Reminders)
+
+	examples[0] = [2]string{"!reminders_set activities Scrim", "Set \"Scrim\" as the only valid reminder activity for this team."}
+	command.AddCommand("reminders_set", "Update reminder config.", examples, RemindersSet)
 }
 
-// Reminders shows the reminder config for this team
+// Reminders shows the reminder config for this team.
 func Reminders(s *state.State, m *discordgo.MessageCreate, args []string) (string, error) {
 	t := s.FindTeam(m.GuildID, m.ChannelID)
 	if t.ID == 0 {
@@ -51,4 +57,53 @@ func Reminders(s *state.State, m *discordgo.MessageCreate, args []string) (strin
 
 	s.Session.ChannelMessageSendEmbed(m.ChannelID, &embed)
 	return "", nil
+}
+
+// RemindersSet updates a field in the reminder config for a team.
+func RemindersSet(s *state.State, m *discordgo.MessageCreate, args []string) (string, error) {
+	t := s.FindTeam(m.GuildID, m.ChannelID)
+	if t.ID == 0 {
+		return "No team in this channel / guild.", nil
+	}
+
+	if len(args) < 3 {
+		return "usage: !reminders_set <field> <value...>", nil
+	}
+
+	var err error
+	switch args[1] {
+	case "activities":
+		sched := s.FindSchedule(m.GuildID, m.ChannelID)
+		if sched == nil {
+			return "No schedule for this team; can't validate activities.", nil
+		}
+
+		activities, parseErr := parseArgs(args[2:], sched.ValidActivities)
+		if parseErr != nil {
+			return err.Error(), nil
+		}
+		_, err = s.DB.Exec("UPDATE reminders SET activities = $1 WHERE team = $2", activities)
+	case "channel":
+		if !isChannel(args[2]) {
+			return "Invalid channel mention.", nil
+		}
+		_, err = s.DB.Exec("UPDATE reminders SET announce_channel = $1 WHERE team = $2", args[2], t.ID)
+	case "role":
+		m, rerr := regexp.MatchString(`\d+`, args[2])
+		if rerr != nil {
+			log.Println(rerr)
+		}
+		if !m {
+			return "Invalid role ID.", nil
+		}
+		_, err = s.DB.Exec("UPDATE reminders SET role_mention = $1 WHERE team = $2", "<@&"+args[2]+">", t.ID)
+	default:
+		return fmt.Sprintf("Invalid field %q, valid options: activities, channel, role", args[2]), nil
+	}
+
+	if err != nil {
+		return fmt.Sprintf("Error updating %s: %s", args[2], err), err
+	}
+
+	return "Updated " + args[1] + ".", nil
 }
